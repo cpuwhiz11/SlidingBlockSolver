42bc59e5cfee56da764205fa882b265bd1b962af
cpuwhiz11 <paul_geromini@student.uml.edu>
2012-11-20 20:05:49 -0500

    Able to move from space to space and have it show on grid

--- src/slidingBlockPuzzleSover/GameWindow.java
+++ src/slidingBlockPuzzleSover/GameWindow.java
@@ -2,12 +2,14 @@ package slidingBlockPuzzleSover;
 
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -227,7 +229,35 @@ public class GameWindow {
 				// Add that to the grid row list
 				//tempBtn.setText(String.valueOf(intArray.get(numCounter))); 
 				//grid[i][j] = tempBtn;
-				grid[i][j] = new JButton(String.valueOf(intArray.get(numCounter))); 
+				JButton numButton = new JButton(String.valueOf(intArray.get(numCounter))); 
+				
+				// Add an action listener to each button
+				// On click try to move the number into the open space
+				numButton.addActionListener(new ActionListener() {
+					 
+		            public void actionPerformed(ActionEvent e)
+		            {
+		            	// Execute solver AI
+		            	Boolean result = 
+		            			moveNumber(Integer.parseInt(((JButton)e.getSource()).getText())); 
+		            	
+		            	if(!result){
+		            		// Bad move
+		            		JDialog warnDialog = new JDialog();
+		            		warnDialog.setSize(450,100);
+		            		warnDialog.setTitle("Warning");
+		            		warnDialog.add(new Label("Bad Move. Select a number directly adjacent to the open space")); 
+		            		warnDialog.setVisible(true); 
+		            	} 
+		            	
+		            	// Valid move, refresh the view
+		            	refreshGridView(); 
+		            }
+
+
+		        });  
+				
+				grid[i][j] = numButton; 
 				this.gridPanel.add(grid[i][j]); 
 				row.add(intArray.get(numCounter)); 
 				numCounter++;
@@ -252,5 +282,154 @@ public class GameWindow {
 	private void refreshView() {
 		this.mainFrame.revalidate(); 
 	}
+
+	/**
+	 * Move a number from one button to the empty space
+	 * @param num, the number trying to be moved
+	 */
+	private boolean moveNumber(int num) {
+		//System.out.print("Number is " + num); 
+		
+		// Get the number grid and look and see if this is a valid move
+		// find the number the player is trying to move
+		int posR = -1, posC = -1;
+		for(int i = 0; i < this.gameList.size(); i++){
+			for(int j = 0; j < this.gameList.get(i).size(); j++){
+				if(this.gameList.get(i).get(j) == num){
+					posR = i;
+					posC = j; 		
+				}
+			}
+		}
+		
+		// Sanity check, should have found a number
+		if(posR == -1 || posC == -1){
+			throw new Error("Did not find the number you clicked!");
+		}
+		
+		// Try to access empty space near found number
+		try {
+			if(this.gameList.get(posR).get(posC + 1) == -1) {
+				swapEmptySpace(posR, posC);
+				return true; 
+			}
+		} catch(Exception e){
+		}
+		try {
+			if(this.gameList.get(posR).get(posC - 1) == -1) {
+				swapEmptySpace(posR, posC);
+				return true; 
+			}
+		} catch(Exception e){
+		}
+		try {
+			if(this.gameList.get(posR + 1).get(posC) == -1) {
+				swapEmptySpace(posR, posC);
+				return true;
+			}
+		} catch(Exception e){
+		}
+		try {
+			if(this.gameList.get(posR - 1).get(posC) == -1) {
+				swapEmptySpace(posR, posC);
+				return true;
+			}
+		} catch(Exception e){
+		}
+		
+		// Was unable to swap, bad move
+		return false;
+	}
+
+	/**
+	 * Swap the empty space with the value a user clicked
+	 * @param posR, row value to be swapped is in
+	 * @param posC, column value to be swapped is in 
+	 */
+	private void swapEmptySpace(int posR, int posC) {
+		// Find -1 which indicates the empty space 
+		int emptyR = -1, emptyC = -1;
+		for(int i = 0; i < this.gameList.size(); i++){
+			for(int j = 0; j < this.gameList.get(i).size(); j++){
+				if(this.gameList.get(i).get(j) == -1){
+					emptyR = i;
+					emptyC = j; 		
+				}
+			}
+		}
+		
+		// Sanity check, should have found empty space
+		if(emptyR == -1 || emptyC == -1){
+			throw new Error("Did not find the empty space!");
+		}
+		
+		// Swap empty space with new number 
+		this.gameList.get(emptyR).set(emptyC, this.gameList.get(posR).get(posC)); 
+		
+		// Swap new number with empty space 
+		this.gameList.get(posR).set(posC, -1); 
+	}
 	
+	/**
+	 * We have moved a piece on the board, rebuild the board
+	 */
+	private void refreshGridView() {
+		
+		// Clear out grid panel
+		this.gridPanel.removeAll(); 
+		
+		// Array of buttons to form the grid
+		JButton[][] grid = new JButton[gameList.size()][gameList.size()]; 
+		
+		
+		for(int i = 0; i < gameList.size(); i++) {			
+
+			for(int j = 0; j < gameList.get(i).size(); j++){				
+				
+				// When the gamelist's num is -1 that is the blank space
+				if(gameList.get(i).get(j) == -1){
+					grid[i][j] = new JButton(" "); 
+					this.gridPanel.add(grid[i][j]); 
+					continue; 
+				}
+				
+				JButton numButton = new JButton(String.valueOf(gameList.get(i).get(j))); 
+				
+				// Add an action listener to each button
+				// On click try to move the number into the open space
+				numButton.addActionListener(new ActionListener() {
+					 
+		            public void actionPerformed(ActionEvent e)
+		            {
+		            	// Execute solver AI
+		            	Boolean result = 
+		            			moveNumber(Integer.parseInt(((JButton)e.getSource()).getText())); 
+		            	
+		            	if(!result){
+		            		// Bad move
+		            		JDialog warnDialog = new JDialog();
+		            		warnDialog.setSize(450,100);
+		            		warnDialog.setTitle("Warning");
+		            		warnDialog.add(new Label("Bad Move. Select a number directly adjacent to the open space")); 
+		            		warnDialog.setVisible(true); 
+		            	} 
+		            	
+		            	// Valid move, refresh the view
+		            	refreshGridView(); 
+		            }
+
+
+		        });  
+				
+				grid[i][j] = numButton; 
+				this.gridPanel.add(grid[i][j]); 
+			}
+			
+		}
+		
+		// Rebuilt grid so refresh view
+		refreshView(); 
+		
+	}
+
 }
